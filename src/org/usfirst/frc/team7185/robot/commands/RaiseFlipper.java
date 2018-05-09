81667066781c86019fb1eb02c8e7f203ea7a2766
Terry Wall <twall@glendora.k12.ca.us>
2018-05-09 14:56:22 -0700

    more tweaks to flipper up and down values

--- src/org/usfirst/frc/team7185/robot/commands/RaiseFlipper.java
+++ src/org/usfirst/frc/team7185/robot/commands/RaiseFlipper.java
@@ -20,7 +20,7 @@ public class RaiseFlipper extends Command {
     protected void initialize() {
     	
     	setTimeout(RobotMap.raiseFlipperInterval);
-    	Robot.flipperSub.set(-.8);
+    	Robot.flipperSub.set(-1);
     	
     
     }
--- src/org/usfirst/frc/team7185/robot/subsystems/FlipperSubsystem.java
+++ src/org/usfirst/frc/team7185/robot/subsystems/FlipperSubsystem.java
@@ -18,7 +18,7 @@ public class FlipperSubsystem extends Subsystem {
 	public FlipperSubsystem(){
 		
 		flipperMotor = new WPI_TalonSRX(RobotMap.flipperDriver);
-		flipperMotor.configOpenloopRamp (3, 2000); 
+		flipperMotor.configOpenloopRamp (1.5, 2000); 
 		
 	}
 	
